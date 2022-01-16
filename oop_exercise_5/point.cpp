43ce58e686402de51df0962793fc56920a7de755
Diana Kolpakova <45013798+Darracuda@users.noreply.github.com>
2022-01-17 00:33:11 +0300

    Add files via upload

---
+++ oop_exercise_5/point.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include "point.h"
+
+using namespace std;
+
+Point::Point()
+{
+	this->x = 0.0;
+	this->y = 0.0;
+}
+
+Point::Point(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+double Point::Distance(const Point& point1, const Point& point2)
+{
+	double dx = point1.x - point2.y;
+	double dy = point1.y - point2.y;
+	double distance = sqrt(dx * dx + dy * dy);
+	return distance;
+}
+
+bool Point::operator==(const Point& other)
+{
+	return (this->x == other.x)
+		&& (this->y == other.y);
+}
+
+istream& operator>>(istream& is, Point& point)
+{
+	is >> point.x >> point.y;
+	return is;
+}
+
+ostream& operator<<(ostream& os, Point& point)
+{
+	os << "(" << point.x << ", " << point.y << ")";
+	return os;
+}
